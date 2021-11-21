616e4db5f443c3a2887e5f8352249289d8ad846b
mckapsata <mkapsata@gmail.com>
2021-11-21 09:26:02 +0200

    adjust log refactor

--- tests/Unit/Adjustments/AdjustLogTest.php
+++ tests/Unit/Adjustments/AdjustLogTest.php
@@ -2,20 +2,33 @@
 
 namespace TNM\CBS\Tests\Unit\Adjustments;
 
+use Illuminate\Support\Facades\Http;
 use TNM\CBS\Responses\AdjustLog\AdjustLogResponse;
+use TNM\CBS\Services\AdjustLog\AdjustLogClient;
 use TNM\CBS\Tests\TestCase;
 
 class AdjustLogTest extends TestCase
 {
- public function test_adjust_log()
- {
-     $stub = file_get_contents(__DIR__ . '/adjust.log.response.xml');
-     $result = (new AdjustLogResponse('QueryAdjustLogResultMsg', 'QueryAdjustLogResult', $stub));
+    public function test_adjust_log()
+    {
+        $stub = file_get_contents(__DIR__ . '/adjust.log.response.xml');
+        $result = (new AdjustLogResponse('QueryAdjustLogResultMsg', 'QueryAdjustLogResult', $stub));
 
-     $adjust = $result->getLogs();
+        $adjust = $result->getLogs();
 
-     $this->assertEquals(1,sizeof($adjust));
+        $this->assertEquals(1, sizeof($adjust));
 
-     $this->assertEquals('100', $adjust[0]['BalanceAdjustmentInfo']['AdjustmentAmt']);
- }
+        $this->assertEquals('100', $adjust[0]['BalanceAdjustmentInfo']['AdjustmentAmt']);
+    }
+
+    public function test_can_query_adjustments()
+    {
+        $stub = file_get_contents(__DIR__ . '/adjust.log.response.xml');
+        Http::fake(['*' => Http::response($stub)]);
+        $result = (new AdjustLogClient('888800900'))->query();
+
+        $this->assertInstanceOf(AdjustLogResponse::class, $result);
+
+        $this->assertEquals('100', $result->getLogs()[0]['BalanceAdjustmentInfo']['AdjustmentAmt']);
+    }
 }
--- tests/Unit/Invoices/InvoicesTest.php
+++ tests/Unit/Invoices/InvoicesTest.php
@@ -2,7 +2,9 @@
 
 namespace TNM\CBS\Tests\Unit\Invoices;
 
+use Illuminate\Support\Facades\Http;
 use TNM\CBS\Responses\Invoices\InvoicesResponse;
+use TNM\CBS\Services\Invoices\InvoicesClient;
 use TNM\CBS\Tests\TestCase;
 
 class InvoicesTest extends TestCase
@@ -14,8 +16,19 @@ class InvoicesTest extends TestCase
 
         $invoices = $result->getInvoices();
 
-        $this->assertEquals(1,sizeof($invoices));
+        $this->assertEquals(1, sizeof($invoices));
 
         $this->assertEquals('190530', $invoices[0]['InvoiceAmount']);
+    }
+
+    public function test_can_query_invoices()
+    {
+        $stub = file_get_contents(__DIR__ . '/invoices.response.xml');
+        Http::fake(['*' => Http::response($stub)]);
+        $result = (new InvoicesClient('888800900'))->query();
+
+        $this->assertInstanceOf(InvoicesResponse::class, $result);
+
+        $this->assertEquals('190530', $result->getInvoices()[0]['InvoiceAmount']);
     }
 }

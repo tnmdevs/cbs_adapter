36c553192aa4c577047243b86bb436f836f95fcd
mckapsata <mkapsata@gmail.com>
2022-06-14 14:07:55 +0200

    query invoices using account

--- src/Services/Invoices/AccountInvoicesClient.php
+++ src/Services/Invoices/AccountInvoicesClient.php
@@ -1,25 +1,20 @@
 <?php
 
-
 namespace TNM\CBS\Services\Invoices;
 
-
 use Carbon\Carbon;
-use TNM\CBS\Responses\CbsResponse;
 use TNM\CBS\Responses\Invoices\InvoicesResponse;
-use TNM\CBS\Services\ICBSClient;
 
-class InvoicesClient implements ICBSClient
+class AccountInvoicesClient
 {
-
-    private string $msisdn;
+    private string $account;
     private ?string $start_time;
     private ?string $end_time;
     private IInvoicesService $service;
 
-    public function __construct(string $msisdn, string $start_time=null, string $end_time=null)
+    public function __construct(string $account, string $start_time=null, string $end_time=null)
     {
-        $this->msisdn = $msisdn;
+        $this->account = $account;
         $this->start_time = $start_time??cbs_time(Carbon::now()->subMonths(3)->firstOfMonth());
         $this->end_time = $end_time??cbs_time(Carbon::now()->addDay());
         $this->service = app(IInvoicesService::class);
@@ -27,11 +22,11 @@ class InvoicesClient implements ICBSClient
 
     public function query(): InvoicesResponse
     {
-        return $this->service->query(['msisdn' => msisdn($this->msisdn)->toCbsFormat(),
-            'account'=>sprintf('<arc:PrimaryIdentity>%s</arc:PrimaryIdentity',msisdn($this->msisdn)->toCbsFormat()),
+        return $this->service->query(['msisdn' =>$this->account,
+            'account'=>sprintf('<arc:AccountCode>%s</arc:AccountCode>',$this->account),
             'start_time' => $this->start_time,
             'end_time'=>$this->end_time],
-        InvoicesResponse::class
+            InvoicesResponse::class
         );
     }
 }
